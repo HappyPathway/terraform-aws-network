08afc107e34bdfad88a59afeeebf57c2dd0d51e5
David Arnold <dja+djaboxx@sonic.net>
2018-10-02 12:50:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
 module "vpc" {
   source       = "github.com/HappyPathway/terraform-aws-vpc"
   version      = "1.0.1"
-  cidr     = "${var.vpc_cidr}"
-  name = "${var.network_name}"
-  azs = ["${var.availability_zone}"]
-  enable_nat_gateway = true
-  public_subnets = ["${var.public_subnet_cidr}"]
-  private_subnets = ["${var.private_subnet_cidr}"]
+  vpc_cidr     = "${var.vpc_cidr}"
+  network_name = "${var.network_name}"
 }
 
 module "public-subnet" {
   source            = "github.com/HappyPathway/terraform-aws-public-subnet"
   version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
-  route_table_id    = "${element(module.vpc.public_route_table_ids, 0)}"
+  route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${var.availability_zone}"
   network_name      = "${var.network_name}"
   subnet_cidr       = "${var.public_subnet_cidr}"
