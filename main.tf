0848c3c27c68916cc4d6ab0a052bd86c2ef49ffc
Dave Arnold <darnold@hashicorp.com>
2018-10-02 14:13:20 -0700

    changing name of subnets to reflect private/public

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "public-subnet1" {
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${element(var.availability_zones, 0)}"
-  network_name      = "${var.network_name}00"
+  network_name      = "${var.network_name}-public00"
   subnet_cidr       = "${element(var.public_subnet_cidrs, 0)}"
 }
 
@@ -21,7 +21,7 @@ module "public-subnet2" {
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${element(var.availability_zones, 1)}"
-  network_name      = "${var.network_name}01"
+  network_name      = "${var.network_name}-public01"
   subnet_cidr       = "${element(var.public_subnet_cidrs, 1)}"
 }
 
@@ -31,7 +31,7 @@ module "private-subnet1" {
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet1.subnet_id}"
   availability_zone = "${element(var.availability_zones, 0)}"
-  network_name      = "${var.network_name}00"
+  network_name      = "${var.network_name}-private00"
   subnet_cidr       = "${element(var.private_subnet_cidrs, 0)}"
 }
   
@@ -41,6 +41,6 @@ module "private-subnet2" {
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet2.subnet_id}"
   availability_zone = "${element(var.availability_zones, 1)}"
-  network_name      = "${var.network_name}01"
+  network_name      = "${var.network_name}-private01"
   subnet_cidr       = "${element(var.private_subnet_cidrs, 1)}"
 }
