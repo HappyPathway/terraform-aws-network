cb3b0b6856706a29e78af753b4599965bf146c39
Dave Arnold <darnold@hashicorp.com>
2018-10-02 14:11:35 -0700

    updating cidrs

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidrs" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
   type = "list"
 }
 
 variable "private_subnet_cidrs" {
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  default = ["10.0.7.0/24", "10.0.8.0/24"]
   type = "list"
 }
 
