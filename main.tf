d3d69c8ae7f23406aa297835231a1703a457a3a3
David Arnold <10138997+djaboxx@users.noreply.github.com>
2021-11-30 09:58:04 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
 
 module "public-subnet1" {
   source  = "app.terraform.io/roknsound/public-subnet/aws"
-  version = "1.0.2"
+  version = "~> 1.0"
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${element(var.availability_zones, 0)}"
@@ -18,7 +18,7 @@ module "public-subnet1" {
 
 module "public-subnet2" {
   source  = "app.terraform.io/roknsound/public-subnet/aws"
-  version = "1.0.2"
+  version = "~> 1.0"
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${element(var.availability_zones, 1)}"
@@ -28,7 +28,7 @@ module "public-subnet2" {
 
 module "private-subnet1" {
   source  = "app.terraform.io/roknsound/private-subnet/aws"
-  version = "1.0.3"
+  version = "~> 1.0"
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet1.subnet_id}"
   availability_zone = "${element(var.availability_zones, 0)}"
@@ -38,7 +38,7 @@ module "private-subnet1" {
   
 module "private-subnet2" {
   source  = "app.terraform.io/roknsound/private-subnet/aws"
-  version = "1.0.3"
+  version = "~> 1.0"
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet2.subnet_id}"
   availability_zone = "${element(var.availability_zones, 1)}"
