d61db545e8193438684d956944025646446be81b
Dave Arnold <dave@roknsound.com>
2024-05-15 16:43:20 -0700

    modernizing module, using count on module calls

--- main.tf
+++ main.tf
@@ -5,38 +5,22 @@ module "vpc" {
   network_name = "${var.network_name}"
 }
 
-module "public-subnet1" {
+module "public-subnet" {
   source  = "HappyPathway/public-subnet/aws"
+  count   = length(var.public_subnet_cidrs)
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
-  availability_zone = "${element(var.availability_zones, 0)}"
-  network_name      = "${var.network_name}-public00"
-  subnet_cidr       = "${element(var.public_subnet_cidrs, 0)}"
+  availability_zone = "${element(var.availability_zones, count.index)}"
+  network_name      = "${var.network_name}-public-${split("/", element(var.public_subnet_cidrs, count.index))[0]}"
+  subnet_cidr       = element(var.public_subnet_cidrs, count.index)
 }
 
-module "public-subnet2" {
-  source  = "HappyPathway/public-subnet/aws"
-  vpc_id            = "${module.vpc.vpc_id}"
-  route_table_id    = "${module.vpc.route_table_id}"
-  availability_zone = "${element(var.availability_zones, 1)}"
-  network_name      = "${var.network_name}-public01"
-  subnet_cidr       = "${element(var.public_subnet_cidrs, 1)}"
-}
-
-module "private-subnet1" {
+module "private-subnet" {
   source  = "HappyPathway/private-subnet/aws"
+  count = length(var.private_subnet_cidrs)
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet1.subnet_id}"
-  availability_zone = "${element(var.availability_zones, 0)}"
+  availability_zone = "${element(var.availability_zones, count.index)}"
   network_name      = "${var.network_name}-private00"
-  subnet_cidr       = "${element(var.private_subnet_cidrs, 0)}"
-}
-  
-module "private-subnet2" {
-  source  = "HappyPathway/private-subnet/aws"
-  vpc_id            = "${module.vpc.vpc_id}"
-  public_subnet_id  = "${module.public-subnet2.subnet_id}"
-  availability_zone = "${element(var.availability_zones, 1)}"
-  network_name      = "${var.network_name}-private01"
-  subnet_cidr       = "${element(var.private_subnet_cidrs, 1)}"
+  subnet_cidr       = "${element(var.private_subnet_cidrs, count.index)}"
 }
--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidrs" {
-  default = ["10.0.5.0/24", "10.0.6.0/24"]
+  default = []
   type = list
 }
 
 variable "private_subnet_cidrs" {
-  default = ["10.0.7.0/24", "10.0.8.0/24"]
+  default = []
   type = list
 }
 
